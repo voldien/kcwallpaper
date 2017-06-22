9df69511d94a54cb741509288f67b3a30d68efe3
Voldie <voldiekami@gmail.com>
2017-06-22 19:07:47 +0200

    Started adding logging level and more logging flexibility.

--- kcw/kcwlog.py
+++ kcw/kcwlog.py
@@ -15,8 +15,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 kcw_verbose = False
+kcw_verbosity = 0
+
+KCW_QUITE = 0
+KCW_VERBOSE = 1
+KCW_DEBUG = 2
 
 
+# Set verbosity level of the program.
+def kcw_set_verbosity(verbosity):
+    global kcw_verbosity
+    kcw_verbosity = verbosity
+
+
+# Check if verbosity is enabled.
 def kcw_is_verbose():
     return kcw_verbose
 
@@ -26,6 +38,24 @@ def kcw_enable_verbose():
     kcw_verbose = True
 
 
-def kcw_verbose_print(cstr):
-    if kcw_is_verbose():
-        print(cstr)
+# Print error.
+def kcw_errorf(fmt, **kwargs):
+    sys.stderr.write(fmt % kwargs)
+#    print(fmt % kwargs, file=sys.stderr, end='\n')
+
+
+#
+def kcw_logf(verbosity, fmt, *kwargs):
+    if kcw_verbose <= verbosity:
+        print(fmt % kwargs)
+
+
+# print
+def kcw_verbose_print(fmt, *kwargs):
+    return kcw_logf(KCW_VERBOSE, fmt, *kwargs)
+
+
+#
+def kcw_debug_printf(fmt, *kwargs):
+    return kcw_logf(KCW_QUITE, fmt % kwargs)
+
