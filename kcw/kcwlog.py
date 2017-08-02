fc2ae478be2c3476c0fbf0e1333f004f57ca0034
voldie <voldiekami@gmail.com>
2017-08-02 17:52:31 +0200

    Improved formating with kwargs and started adding documentation for the functions and minor cleanup.

--- kcw/kcwlog.py
+++ kcw/kcwlog.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
 #
 KCW_QUITE = 0
 KCW_VERBOSE = 1
@@ -25,37 +27,69 @@ kcw_verbosity = KCW_QUITE
 
 # Set verbosity level of the program.
 def kcw_set_verbosity(verbosity):
+    """
+    Set logging verbosity level. The higher the
+    more information gets logged.
+    :param verbosity:
+    :return:
+    """
     global kcw_verbosity
     kcw_verbosity = verbosity
 
+
 def kcw_get_verbosity():
+    """
+    Get current verbosity level of the program
+    :return: verbosity level as int.
+    """
+    global kcw_verbosity
     return kcw_verbosity
 
-# Print error.
-def kcw_errorf(fmt, **kwargs):
-    sys.stderr.write(fmt % kwargs)
-#    print(fmt % kwargs, file=sys.stderr, end='\n')
+
+def kcw_errorf(fmt, *kwargs):
+    """
+    Print error format. The string will be written
+    into stderr rather than the default with print.
+    :param fmt: format string.
+    :param kwargs: list of arguments
+    :return: number of character written.
+    """
+    return sys.stderr.write(fmt.format(*kwargs))
 
 
-#
 def kcw_logf(verbosity, fmt, *kwargs):
     """
-
-    :param verbosity:
-    :param fmt:
-    :param kwargs:
+    Log format message based on the verbosity.
+    :param verbosity: verbosity of the message.
+    :param fmt: format message.
+    :param kwargs: argument for fmt.
     :return:
     """
+    nchar = 0
     if kcw_get_verbosity() >= verbosity:
-        print(fmt % kwargs)
+        log = fmt.format(*kwargs)
+        nchar = len(log)
+        print(log)
+
+    return nchar
 
 
-# print
-def kcw_verbose_print(fmt, *kwargs):
+def kcw_verbose_printf(fmt, *kwargs):
+    """
+    Verbose logging.
+    :param fmt:
+    :param kwargs:
+    :return: number of character.
+    """
     return kcw_logf(KCW_VERBOSE, fmt, *kwargs)
 
 
-#
 def kcw_debug_printf(fmt, *kwargs):
-    return kcw_logf(KCW_DEBUG, fmt % kwargs)
+    """
+    Debug logging.
+    :param fmt:
+    :param kwargs:
+    :return: number of character.
+    """
+    return kcw_logf(KCW_DEBUG, fmt, *kwargs)
 
