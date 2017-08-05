316c01d97d71988ea25f9363711c18ef0c8eb6e1
voldie <voldiekami@gmail.com>
2017-08-05 20:00:45 +0200

    Improved abstract class, added more documentation, added sql connection variable to base class.

--- kcw/db/kcwsql.py
+++ kcw/db/kcwsql.py
@@ -13,52 +13,137 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import abc
 
-class SqlConnection:
-    """"""
 
-#    con = None
-#    enabled = True
+class SqlConnection(object):
+    """
+    Abstract base class for SQL
+    connection
+    """
 
-    #
     def __init__(self):
         pass
 
-    #
-    def is_enabled(self):
+    @abc.abstractmethod
+    def execute_command(self, query):
+        """
+        Execute internal query command.
+        :param query: query string.
+        :return:
+        """
         pass
 
-    # Execute SQL command.
-    def kcw_mysql_execute_command(self, query):
-        pass
-
-    # Connect to mysql server.
+    @abc.abstractmethod
     def connect(self, user, password, host, port, database):
+        """
+        Connect to sql server.
+        :param user: username.
+        :param password: password
+        :param host: host either as Dot-decimal notation or FQDN.
+        :param port: port on server.
+        :param database: database schema.
+        :return: none false if successfully.
+        """
         pass
 
-    #
+    @abc.abstractmethod
     def disconnect(self):
+        """
+        Disconnect from server.
+        :return:
+        """
         pass
-    #
+
+    def get_sqlcon(self):
+        """
+
+        :return:
+        """
+        return self.sql_connection
+
+    def set_sqlcon(self, connection):
+        """
+
+        :param connection:
+        :return:
+        """
+        self.sql_connection = connection
+
+    @abc.abstractmethod
     def clear_cache(self, table):
+        """
+        Clear all cache stored on the sql database.
+        :param table:
+        :return: none false if successfully.
+        """
         pass
 
-    # Check if table has been created.
+    @abc.abstractmethod
     def check_table_exists(self, table):
+        """
+        Check if table exists in sql schema.
+        :param table: name of table
+        :return: none false if exists.
+        """
         pass
 
+    @abc.abstractmethod
     def check_img_exists(self, table, imgid):
+        """
+        Check if image exist in table.
+        :param table: table to lookup at.
+        :param imgid: image unique ID.
+        :return: none false if image exists.
+        """
         pass
 
+    @abc.abstractmethod
     def num_entries_by_table(self, table):
+        """
+        Query number of entries in table.
+        :param table: table in sql database.
+        :return: number of entries.
+        """
         pass
 
+    @abc.abstractmethod
     def add_img_entry(self, table, url, preview, score, imgid, tags):
+        """
+        Add image to sql.
+        :param table:
+        :param url:
+        :param preview:
+        :param score:
+        :param imgid:
+        :param tags:
+        :return: non-false if successful.
+        """
         pass
 
+    @abc.abstractmethod
     def get_cached_img_url_by_id(self, table, imgid):
+        """
+        Get cached image url by image unique ID.
+        :param table:
+        :param imgid:
+        :return: non-empty string.
+        """
         pass
 
+    @abc.abstractmethod
     def get_cached_img_url_by_tag(self, table, col, tag, offset=0):
+        """
+        Get cached image local directory path by the tag
+        associated with the image.
+        :param table: table to search on.
+        :param col:
+        :param tag: tags associated with image.
+        :param offset: nth result.
+        :return: non-empty string.
+        """
         pass
+
+    sql_connection = None
+    connection = property(get_sqlcon, set_sqlcon)
 
