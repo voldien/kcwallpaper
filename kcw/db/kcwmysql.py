7ccd50b51955176cde7027e7101e3511f1833e60
voldie <voldiekami@gmail.com>
2017-08-05 21:39:40 +0200

    Code cleanup and other minor changes.

--- kcw/db/kcwmysql.py
+++ kcw/db/kcwmysql.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import mysql.connector
+import kcw
 from kcwsql import *
 from dbdef import *
 import time
@@ -28,28 +29,18 @@ class MySqlConnection (SqlConnection):
 
     #
     def __init__(self):
-        self.con = None
-        self.enabled = True
+        super(MySqlConnection, self).__init__()
+        pass
 
-    #
-    def is_enabled(self):
-        return self.enabled
-
-    # Execute SQL command.
-    def kcw_mysql_execute_command(self, query):
+    def execute_command(self, query):
         try:
-            cur = self.con.cursor()
+            cur = self.connection.cursor()
             cur.execute(query)
             res = cur.fetchone()
             cur.close()
-            del cur
             return res
         except mysql.connector.Error as err:
-            print(err.message)
-        except mysql.connector.InternalError as err:
-            print(err.message)
-        except Exception as err:
-            print(err.message)
+            kcw.kcw_errorf("Failed to perform query '{}'\n\terror : {}.\n", query, err.message)
 
         return None
 
@@ -61,70 +52,54 @@ class MySqlConnection (SqlConnection):
                                                  database=database)
             return self.con
         except mysql.connector.Error as err:
-            print("Couldn't connect to a mysql server %s:%d %s." % (host, port, err.message))
+            print("Couldn't connect to a mysql server %s:%d %s." % (host, port, err.msg))
             print("Caching can not be used without MySQL.")
         except Exception as err:
             print("Couldn't connect to mysql, %s." % err.message)
         return None
 
-    #
+        raise Exception("Failed to connect.\n")
+
     def disconnect(self):
         if self.con:
             self.con.close()
 
-    #
     def clear_cache(self, table):
-        if not self.is_enabled():
-            return
 
-        self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_TRUNCATE.format(table))
+        self.execute_command(SQL_FORMAT_QUERY_TRUNCATE.format(table))
 
-    # Check if table has been created.
     def check_table_exists(self, table):
-        if not self.is_enabled():
-            return False
 
-        res = self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_TABLE_EXIST.format(table))
+        res = self.execute_command(SQL_FORMAT_QUERY_TABLE_EXIST.format(table))
 
         return res is not None
 
-    #
     def check_img_exists(self, table, imgid):
-        if not self.is_enabled():
-            return False
 
-        res = self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_CHECK_IMG_EXISTS)
+        res = self.execute_command(SQL_FORMAT_QUERY_CHECK_IMG_EXISTS)
+
         return res is not None
 
-    #
     def num_entries_by_table(self, table):
-        if not self.is_enabled():
-            return False
 
-        res = self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_NUM_ENTRIES_IN_TABLE.format(table))
+        res = self.execute_command(SQL_FORMAT_QUERY_NUM_ENTRIES_IN_TABLE.format(table))
 
         if res:
             return res[0]
         else:
             return 0
 
-    #
     def add_img_entry(self, table, url, preview, score, imgid, tags):
-        if not self.is_enabled():
-            return False
 
-        res = self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_ADD_IMG_ENTRY.format(
+        res = self.execute_command(SQL_FORMAT_QUERY_ADD_IMG_ENTRY.format(
             table, url, preview, score, imgid, tags, time.time()))
 
         return res is not None
 
-    #
     def get_cached_img_url_by_id(self, table, imgid):
-        if not self.is_enabled():
-            return None
 
         #
-        res = self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_IMG_BY_IMGID.format(table, imgid))
+        res = self.execute_command(SQL_FORMAT_QUERY_IMG_BY_IMGID.format(table, imgid))
 
         # Check result.
         if res:
@@ -132,12 +107,9 @@ class MySqlConnection (SqlConnection):
         else:
             return None
 
-    #
     def get_cached_img_url_by_tag(self, table, col, tag, offset=0):
-        if not self.is_enabled():
-            return None
 
-        res = self.kcw_mysql_execute_command(SQL_FORMAT_QUERY_IMG_BY_TAG.format(col, table, tag, offset))
+        res = self.execute_command(SQL_FORMAT_QUERY_IMG_BY_TAG.format(col, table, tag, offset))
 
         # Check result.
         if res:
