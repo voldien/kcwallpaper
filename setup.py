7edb35ec5dc3dfd2aade8d6224847eaacd0a77ad
voldie <voldiekami@gmail.com>
2017-08-09 08:48:45 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import setup, find_packages
+
+if "install" in sys.argv[1]:
+
+    lib_paths = [get_python_lib()]
+    if lib_paths[0].startswith("/usr/lib"):
+        lib_paths.append(get_python_lib(prefix="/usr/local/"))
+    for paths in lib_paths:
+        existing_path = os.path.abspath(os.path.join(lib_paths, "kcw"))
+        if os.path.exists(existing_path):
+            break
+
+version = __import__('kcw').get_version()
+
+EXCLUDE_FROM_PACKAGES = ['']
+
+with open('README.md') as f:
+    readme = f.read()
+    f.close()
+
+setup(
+    name="kcwallpaper",
+    version=version,
+    url="https://github.com/voldien/kcwallpaper",
+    author="Valdemar Lindberg",
+    author_email="voldiekami@gmail.com",
+    description=readme,
+    license="GPL",
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    entry_points={'console_scripts': [
+              'my_project = kcw.__main__:main'
+          ]},
+    install_requires=['mysql-connector-python',
+                      'pysqlite',
+                      'urllib2'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: KCW',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved ::  GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: SQL',
+    ],
+
+)
