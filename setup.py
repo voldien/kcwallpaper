1d01e30a26f71ae1277a77b1c7c7bfa0d3f8fe9e
voldie <voldiekami@gmail.com>
2017-08-16 10:35:12 +0200

    Added additional module requirement.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
           ]},
     install_requires=['mysql-connector-python',
                       'pysqlite',
-                      'urllib2'],
+                      'urllib2', 'mysql'],
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
