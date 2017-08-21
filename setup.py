0fb3f869219e5d2c986fa2e19c2d2ce33d164b6e
Voldie <voldiekami@gmail.com>
2017-08-21 15:49:00 +0200

    Minor.

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
     entry_points={'console_scripts': [
               'kcwallpaper = kcw.__main__:main'
           ]},
-    install_requires=['mysql-connector-python',
+    install_requires=['mysql', 'mysql-connector-python',
                       'pysqlite',
-                      'urllib2', 'mysql'],
+                      'urllib2'],
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
